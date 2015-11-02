2dff6c9770e211f972511c261b01aeb633dc7eac
Mirko Calvaresi <mccalv@gmail.com>
2015-11-02 23:47:37 +0100

    Changed the schema file in order to support null folder

--- ginema-api/src/test/java/com/ginema/api/serialization/SerializationTest.java
+++ ginema-api/src/test/java/com/ginema/api/serialization/SerializationTest.java
@@ -16,13 +16,20 @@
  */
 package com.ginema.api.serialization;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.UUID;
 
+import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
+import org.junit.Test;
 
 import com.ginema.api.avro.DateEntry;
 import com.ginema.api.avro.SensitiveDataHolder;
@@ -32,23 +39,50 @@ import com.ginema.api.avro.SensitiveDataHolder;
  *
  */
 public class SerializationTest {
+  private final static File AVRO_SERIALIZED_FILE = new File("sensitiveDataHolder.avro");
+  private final static Date DATE = new Date();
+  private final static String ID_GENERATED = UUID.randomUUID().toString();
+  private final static String ID_HOLDER = UUID.randomUUID().toString();
+  private final static String DOMAIN = "domain";
 
-  private File file;
-
+  @Test
   public void testSerialization() throws Exception {
     // Deserialize users from disk
     SensitiveDataHolder sensitiveDataHolder = new SensitiveDataHolder();
-    sensitiveDataHolder.setId("id");
-    sensitiveDataHolder.setDomain("domain");
-    sensitiveDataHolder.getDates().put("dd", new DateEntry(UUID.randomUUID().toString(), new Date().getTime()));
+    sensitiveDataHolder.setId(ID_HOLDER);
+    sensitiveDataHolder.setDomain(DOMAIN);
+    sensitiveDataHolder.setDates(new HashMap<String, DateEntry>());
+    sensitiveDataHolder.getDates().put(ID_GENERATED, new DateEntry(ID_GENERATED, DATE.getTime()));
     DatumWriter<SensitiveDataHolder> userDatumWriter =
         new SpecificDatumWriter<SensitiveDataHolder>(SensitiveDataHolder.class);
     DataFileWriter<SensitiveDataHolder> dataFileWriter =
         new DataFileWriter<SensitiveDataHolder>(userDatumWriter);
-     dataFileWriter.create(sensitiveDataHolder.getSchema(), new File("users.avro"));
+    dataFileWriter.create(sensitiveDataHolder.getSchema(), AVRO_SERIALIZED_FILE);
+
+    dataFileWriter.append(sensitiveDataHolder);
     dataFileWriter.close();
 
 
+    DatumReader<SensitiveDataHolder> userDatumReader =
+        new SpecificDatumReader<SensitiveDataHolder>(SensitiveDataHolder.class);
+    DataFileReader<SensitiveDataHolder> dataFileReader =
+        new DataFileReader<SensitiveDataHolder>(AVRO_SERIALIZED_FILE, userDatumReader);
+    SensitiveDataHolder sensitideDataHolder = null;
+    while (dataFileReader.hasNext()) {
+      // Reuse user object by passing it to next(). This saves us from
+      // allocating and garbage collecting many objects for files with
+      // many items.
+      sensitideDataHolder = dataFileReader.next();
+      assertEquals(ID_HOLDER, sensitideDataHolder.getId().toString());
+      assertEquals(DOMAIN, sensitideDataHolder.getDomain().toString());
+
+      assertEquals(DATE, new Date(sensitideDataHolder.getDates().get(ID_GENERATED).getV()));
+
+      System.out.println(sensitideDataHolder);
+    }
+
+    AVRO_SERIALIZED_FILE.delete();
+    dataFileReader.close();
 
   }
 
