73d7e42a89d575eab0c32f17a24a568ef888ae30
Mirko Calvaresi <mccalv@gmail.com>
2015-11-03 23:40:36 +0100

    Added Schema Helper used to produce the JSON schema

--- ginema-api/src/main/java/com/ginema/api/avro/util/SchemaHelper.java
+++ ginema-api/src/main/java/com/ginema/api/avro/util/SchemaHelper.java
@@ -384,7 +384,7 @@ public class SchemaHelper {
   public static Schema getGinemaSchemaFromClassPath() throws Exception {
     InputStream resourceAsStream =
         ClassLoader.getSystemResourceAsStream("avro/sensitivedataHolder.avsc");
-        System.out.print(ClassLoader.getSystemResource("avro/sensitivedataHolder.avsc").getPath());
+        System.out.print("AvroSchema loading:"+ClassLoader.getSystemResource("*").getPath() +ClassLoader.getSystemResource("avro/sensitivedataHolder.avsc").getPath());
     return new Schema.Parser().parse(resourceAsStream);
 
   }
